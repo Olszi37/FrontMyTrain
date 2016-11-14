416341f24ee1995dfaddd489916d1c73fb4607cd
Olszi37 <olszi73@wp.pl>
2016-11-14 19:16:00 +0100

    Added getting row count info

--- WebContent/js/adminScript.js
+++ WebContent/js/adminScript.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+	//getTablesStatus();
+
 	$(document).ajaxStart( function() {
 		$.blockUI({ message: "<div style='background-color: #C6CDF7'>" +
 		"<p><b>Praca serwera w toku</b></p><br>" +
@@ -15,13 +17,81 @@ $(document).ready(function () {
 	});
 });
 
+function getTablesStatus(){
+	getTrainsetStatus();
+	getCarriageStatus();
+	getStationStatus();
+	getCourseStatus();
+	getRoutePointStatus();
+}
+
+function getTrainsetStatus(){
+	var url = "http://localhost:8080/MyTrain/trainset/get/size";
+	var info = $("#trainsetP");
+	var statusField = $("#trainsetStatus");
+
+	sendRowCountRequest(url, info, statusField);
+}
+
+function getCarriageStatus(){
+	var url = "http://localhost:8080/MyTrain/carriage/get/size";
+	var info = $("#carriageP");
+	var statusField = $("#carriageStatus");
+
+	sendRowCountRequest(url, info, statusField);
+}
+
+function getStationStatus() {
+	var url = "http://localhost:8080/MyTrain/station/get/size";
+	var info = $("#stationP");
+	var statusField = $("#stationStatus");
+
+	sendRowCountRequest(url, info, statusField);
+}
+
+function getCourseStatus() {
+	var url = "http://localhost:8080/MyTrain/course/get/size";
+	var info = $("#courseP");
+	var statusField = $("#courseStatus");
+
+	sendRowCountRequest(url, info, statusField);
+}
+
+function getRoutePointStatus(){
+	var url = "http://localhost:8080/MyTrain/routePoint/get/size";
+	var info = $("#routePointP");
+	var statusField = $("#routePointStatus");
+
+	sendRowCountRequest(url, info, statusField);
+}
+
+function sendRowCountRequest(url, info, statusField) {
+	$.ajax({
+		url: url,
+		type: "GET",
+		cache: false,
+		crossDomain: true,
+		headers: {"Access-Control-Allow-Origin": "*"},
+		contentType: false,
+		processData: false,
+		success: function (response, status, jqXHR) {
+			statusField.text(response + " rekordów");
+		},
+		error: function () {
+			info.addClass("error").text("Wystapil blad");
+		}
+	});
+}
+
 function setTrainsets(){
 	var url = "http://localhost:8080/MyTrain/trainset/set/file";
 	var input = $("#trainsetInput").prop("files");
 	var info = $("#trainsetP");
 	var fileName = "trainsets.xlsx";
 
-	sendRequest(url, input, fileName, info);
+	sendFileRequest(url, input, fileName, info);
+
+	getTrainsetStatus();
 }
 
 function setCarriages(){
@@ -30,7 +100,9 @@ function setCarriages(){
 	var info = $("#carriageP");
 	var fileName = "carriages.xlsx";
 
-	sendRequest(url, input, fileName, info);
+	sendFileRequest(url, input, fileName, info);
+
+	getCarriageStatus();
 }
 
 function setStations(){
@@ -39,7 +111,10 @@ function setStations(){
 	var info = $("#stationP");
 	var fileName = "stations.xlsx";
 
-	sendRequest(url, input, fileName, info)}
+	sendFileRequest(url, input, fileName, info);
+
+	getStationStatus();
+}
 
 function setCourses(){
 	var url = "http://localhost:8080/MyTrain/course/set/file";
@@ -47,7 +122,10 @@ function setCourses(){
 	var info = $("#courseP");
 	var fileName = "courses.xlsx";
 
-	sendRequest(url, input, fileName, info)}
+	sendFileRequest(url, input, fileName, info);
+
+	getCourseStatus();
+}
 
 function setRoutePoints(){
 	var url = "http://localhost:8080/MyTrain/routePoint/set/file";
@@ -55,9 +133,12 @@ function setRoutePoints(){
 	var info = $("#routePointP");
 	var fileName = "routePoints.xlsx";
 
-	sendRequest(url, input, fileName, info)}
+	sendFileRequest(url, input, fileName, info);
 
-function sendRequest(url, input, fileName, info){
+	getRoutePointStatus();
+}
+
+function sendFileRequest(url, input, fileName, info){
 
 	if(input.length !== 0){
 		var formData = new FormData();
@@ -75,7 +156,7 @@ function sendRequest(url, input, fileName, info){
 			success: function (response, status, jqXHR) {
 				info.text("Dodano " + response + " rekordów.");
 			},
-			error: function () {
+			error: function (jqXHR, status, errorThrown) {
 				info.addClass("error").text("Wystąpił błąd");
 			}
 		});
