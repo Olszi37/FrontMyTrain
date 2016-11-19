162ee2200292e9600e0c3d28b20620af91b22116
Olszi37 <olszi73@wp.pl>
2016-11-19 01:25:07 +0100

    beggining of table edition

---
+++ WebContent/js/editTableScript.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Siny on 2016-11-19.
+ */
+
+function editTrainset() {
+    var createUrl = "http://localhost:8080/MyTrain/trainset";
+    var updateUrl = "http://localhost:8080/MyTrain/trainset";
+    var deleteUrl = "http://localhost:8080/MyTrain/trainset";
+    var getAllUrl = "http://localhost:8080/MyTrain/trainset/getll";
+}
+
+function editCarriage() {
+    var createUrl;
+    var updateUrl;
+    var deleteUrl;
+    var getAllUrl;
+}
+
+function editStation() {
+    var createUrl;
+    var updateUrl;
+    var deleteUrl;
+    var getAllUrl;
+}
+
+function editCourse() {
+    var createUrl;
+    var updateUrl;
+    var deleteUrl;
+    var getAllUrl;
+}
+
+function editRoutePoint() {
+    var createUrl;
+    var updateUrl;
+    var deleteUrl;
+    var getAllUrl;
+}
+
+
+
+function sendDeleteRequest() {
+
+}
+
+function sendUptadeRequest() {
+
+}
+
+function sendAddRequest() {
+
+}
+
+function sendFindRequest() {
+
+}
+
+function sendGetAllRequest() {
+
+}
